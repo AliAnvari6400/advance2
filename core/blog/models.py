fe52a7aa54fba16622393f05780207aceeb88633
ALi <a_anvari@ymail.com>
2025-07-22 14:33:11 +0330

    set blog and accounts models, set admin.py for accounts

--- core/blog/models.py
+++ core/blog/models.py
@@ -1,3 +1,24 @@
 from django.db import models
-
+from accounts.models import User
 # Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=250)
+    def __str__(self):
+        return self.name
+    
+class Post(models.Model):
+    # Create model for Post
+    title = models.CharField(max_length=250)
+    content = models.TextField()
+    status = models.BooleanField()
+    category = models.ForeignKey(Category,on_delete=models.SET_NULL,null=True)
+    #image = models.ImageField(blank=True,null=True)
+    author = models.ForeignKey(User,on_delete=models.CASCADE)
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
+    published_date = models.DateTimeField()
+    def __str__(self):
+        return self.title
+
+
