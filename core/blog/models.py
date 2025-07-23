7e09ac9be0bdbff2c88bab00569929582371c200
ALi <a_anvari@ymail.com>
2025-07-23 14:33:56 +0330

    set static & media & blog model

--- core/blog/models.py
+++ core/blog/models.py
@@ -1,5 +1,7 @@
 from django.db import models
-from accounts.models import User
+#from accounts.models import User
+from django.contrib.auth import get_user_model
+user = get_user_model()
 # Create your models here.
 
 class Category(models.Model):
@@ -13,8 +15,8 @@ class Post(models.Model):
     content = models.TextField()
     status = models.BooleanField()
     category = models.ForeignKey(Category,on_delete=models.SET_NULL,null=True)
-    #image = models.ImageField(blank=True,null=True)
-    author = models.ForeignKey(User,on_delete=models.CASCADE)
+    image = models.ImageField(blank=True,null=True)
+    author = models.ForeignKey(user,on_delete=models.CASCADE)
     created_date = models.DateTimeField(auto_now_add=True)
     updated_date = models.DateTimeField(auto_now=True)
     published_date = models.DateTimeField()
